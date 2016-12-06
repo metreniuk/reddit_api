2e2755691f61a9b48b9efb4f932421926590d13e
Alexandr Metreniuc <metreniuk@gmail.com>
2016-12-06 16:58:54 +0200

    added posts recursion

--- dist/index.js
+++ dist/index.js
@@ -14,12 +14,12 @@ request(baseLink + '/subreddits.json', function (error, response, body) {
 
   if (!error && response.statusCode == 200) {
     var res = JSON.parse(body);
-    if (catgsCount > 25) {}
     for (var i = 0; i < catgsCount; i++) {
       catgs.push({
         title: res.data.children[i].data.title,
         url: baseLink + res.data.children[i].data.url,
-        posts: []
+        posts: [],
+        after: res.data.after
       });
     }
 
@@ -31,62 +31,10 @@ request(baseLink + '/subreddits.json', function (error, response, body) {
       var _iteratorError = undefined;
 
       try {
-        var _loop = function _loop() {
+        for (var _iterator = catgs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
           var catg = _step.value;
 
-          request(catg.url + '.json', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              var _res = JSON.parse(body);
-
-              for (var _i = 0; _i < postsCount; _i++) {
-                catg.posts.push({
-                  title: _res.data.children[_i].data.title,
-                  url: baseLink + _res.data.children[_i].data.permalink
-                });
-              }
-
-              if (catg.posts) {
-                console.log(catg);
-              }
-
-              var data = "";
-              var html = '<html><head><title>' + catg.title + '</title><body><ul>';
-
-              var _iteratorNormalCompletion2 = true;
-              var _didIteratorError2 = false;
-              var _iteratorError2 = undefined;
-
-              try {
-                for (var _iterator2 = catg.posts[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                  var post = _step2.value;
-
-                  data += JSON.stringify(post);
-                  html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
-                }
-              } catch (err) {
-                _didIteratorError2 = true;
-                _iteratorError2 = err;
-              } finally {
-                try {
-                  if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                    _iterator2.return();
-                  }
-                } finally {
-                  if (_didIteratorError2) {
-                    throw _iteratorError2;
-                  }
-                }
-              }
-
-              fs.writeFile(path + catg.title + '.html', html, function (err, data) {
-                if (err) console.log(err.message);
-              });
-            }
-          });
-        };
-
-        for (var _iterator = catgs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          _loop();
+          customRequest(catg, postsCount, '');
         }
       } catch (err) {
         _didIteratorError = true;
@@ -107,3 +55,64 @@ request(baseLink + '/subreddits.json', function (error, response, body) {
     console.log("Couldnt' connect\n" + error.message);
   }
 });
+
+function customRequest(catg, remained, after) {
+  console.log(catg.url + '.json?after=' + after);
+  request(catg.url + '.json?after=' + after, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var res = JSON.parse(body);
+
+      var k = remained > 25 ? 25 : remained;
+      console.log(remained);
+
+      for (var i = 0; i < k; i++) {
+
+        catg.posts.push({
+          title: res.data.children[i].data.title,
+          url: baseLink + res.data.children[i].data.permalink,
+          after: res.data.after
+        });
+      }
+      if (remained > 25) customRequest(catg, remained - 25, res.data.after);
+
+      // if (catg.posts) {
+      // 	console.log(catg)
+      // }
+
+      var data = "";
+      var html = '<html><head><title>' + catg.title + '</title><body><ol>';
+
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = catg.posts[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var post = _step2.value;
+
+          data += JSON.stringify(post);
+          html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
+        }
+      }
+
+      html += '</ol></body></html>';
+
+      fs.writeFile(path + catg.title + '.html', html, function (err, data) {
+        if (err) console.log(err.message);
+      });
+    }
+  });
+}
