5b9fbdbc4cf7456bdae133cba31cdacc04a8a156
Alexandr Metreniuc <metreniuk@gmail.com>
2016-12-06 18:18:54 +0200

    implemented catgs pagination

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var fs = require('fs');
+var fs = require('fs-extra');
 var request = require('request');
 var path = "../files/";
 
@@ -10,109 +10,131 @@ var postsCount = process.argv[3];
 var catgs = [];
 var posts = [];
 
-request(baseLink + '/subreddits.json', function (error, response, body) {
+catgsCustomRequest(catgsCount, '');
 
-  if (!error && response.statusCode == 200) {
-    var res = JSON.parse(body);
-    for (var i = 0; i < catgsCount; i++) {
-      catgs.push({
-        title: res.data.children[i].data.title,
-        url: baseLink + res.data.children[i].data.url,
-        posts: [],
-        after: res.data.after
-      });
-    }
+function postsCustomRequest(catg, remained, after) {
+    //console.log(catg.url + '.json?after=' + after)
+    request(catg.url + '.json?after=' + after, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var res = JSON.parse(body);
 
-    if (catgs) {
-      //console.log(catgs)
+            var k = remained > 25 ? 25 : remained;
+            //console.log(remained)
 
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+            for (var i = 0; i < k; i++) {
+                catg.posts.push({
+                    title: res.data.children[i].data.title,
+                    url: res.data.children[i].data.url,
+                    after: res.data.after
+                });
+            }
 
-      try {
-        for (var _iterator = catgs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var catg = _step.value;
+            if (remained > 25) postsCustomRequest(catg, remained - 25, res.data.after);
+            //console.log(remained)
 
-          customRequest(catg, postsCount, '');
+            // if (catg.posts) {
+            // 	console.log(catg)
+            // }
+
+            var data = "";
+            var html = '<html><head><title>' + catg.title + '</title><body><ol>';
+
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = catg.posts[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var post = _step.value;
+
+                    data += JSON.stringify(post);
+                    html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+
+            html += '</ol></body></html>';
+
+            fs.writeFile(path + catg.title + '.html', html, function (err, data) {
+                if (err) console.log(err.message);
+                //    else console.log(catg.title)
+            });
         }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
+    });
+}
+
+function catgsCustomRequest(remained, after) {
+    request(baseLink + '/subreddits.json?after=' + after, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            clearFolder(path);
+            var res = JSON.parse(body);
+
+            var k = remained > 25 ? 25 : remained;
+            //console.log(remained)
+
+            for (var i = 0; i < k; i++) {
+                catgs.push({
+                    title: res.data.children[i].data.url.split('/')[2],
+                    url: baseLink + res.data.children[i].data.url,
+                    posts: [],
+                    after: res.data.after
+                });
+            }
+
+            if (remained > 25) catgsCustomRequest(remained - 25, res.data.after);
+
+            if (catgs) {
+                //console.log(catgs)
+
+                var _iteratorNormalCompletion2 = true;
+                var _didIteratorError2 = false;
+                var _iteratorError2 = undefined;
+
+                try {
+                    for (var _iterator2 = catgs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                        var catg = _step2.value;
+
+                        postsCustomRequest(catg, postsCount, '');
+                    }
+                } catch (err) {
+                    _didIteratorError2 = true;
+                    _iteratorError2 = err;
+                } finally {
+                    try {
+                        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                            _iterator2.return();
+                        }
+                    } finally {
+                        if (_didIteratorError2) {
+                            throw _iteratorError2;
+                        }
+                    }
+                }
+            }
+        } else {
+            console.log("Couldnt' connect\n" + error.message);
         }
-      }
-    }
-  } else {
-    console.log("Couldnt' connect\n" + error.message);
-  }
-});
+    });
+}
 
-function customRequest(catg, remained, after) {
-  console.log(catg.url + '.json?after=' + after);
-  request(catg.url + '.json?after=' + after, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var res = JSON.parse(body);
+function clearFolder(path) {
+    fs.removeSync(path);
 
-      var k = remained > 25 ? 25 : remained;
-      console.log(remained);
-
-      for (var i = 0; i < k; i++) {
-
-        catg.posts.push({
-          title: res.data.children[i].data.title,
-          url: baseLink + res.data.children[i].data.permalink,
-          after: res.data.after
-        });
-      }
-      if (remained > 25) customRequest(catg, remained - 25, res.data.after);
-
-      // if (catg.posts) {
-      // 	console.log(catg)
-      // }
-
-      var data = "";
-      var html = '<html><head><title>' + catg.title + '</title><body><ol>';
-
-      var _iteratorNormalCompletion2 = true;
-      var _didIteratorError2 = false;
-      var _iteratorError2 = undefined;
-
-      try {
-        for (var _iterator2 = catg.posts[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-          var post = _step2.value;
-
-          data += JSON.stringify(post);
-          html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
-        }
-      } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion2 && _iterator2.return) {
-            _iterator2.return();
-          }
-        } finally {
-          if (_didIteratorError2) {
-            throw _iteratorError2;
-          }
-        }
-      }
-
-      html += '</ol></body></html>';
-
-      fs.writeFile(path + catg.title + '.html', html, function (err, data) {
-        if (err) console.log(err.message);
-      });
-    }
-  });
+    fs.mkdirs(path, function (err) {
+        if (err) return console.error(err);
+        console.log(path + " created!");
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const request = require('request')
 const path = "../files/"
 
@@ -8,50 +8,28 @@ let postsCount = process.argv[3]
 let catgs = []
 let posts = []
 
-request(baseLink + '/subreddits.json', (error, response, body) => {
+catgsCustomRequest(catgsCount, '')
 
-  if (!error && response.statusCode == 200) {
-    let res = JSON.parse(body)
-    for (let i = 0; i < catgsCount; i++) {
-    	catgs.push({
-    		title: res.data.children[i].data.title,
-    		url: baseLink + res.data.children[i].data.url,
-    		posts: [],
-    		after: res.data.after
-    	})
-    }
-
-    if (catgs) {
-    	//console.log(catgs)
-    	
-    	for (let catg of catgs) {
-    		customRequest(catg, postsCount, '')
-    	}
-    }
-  } else {
-  	console.log("Couldnt' connect\n" + error.message)
-  }
-})
-
-function customRequest (catg, remained, after) {
-	console.log(catg.url + '.json?after=' + after)
+function postsCustomRequest (catg, remained, after) {
+	//console.log(catg.url + '.json?after=' + after)
 	request(catg.url + '.json?after=' + after, (error, response, body) => {
     			if (!error && response.statusCode == 200) {
     				let res = JSON.parse(body)
     				
     				let k = remained > 25 ? 25: remained
-    				console.log(remained)
+    				//console.log(remained)
 
     				for (let i = 0; i < k; i++) {
-    					
     					catg.posts.push({
     						title: res.data.children[i].data.title,
-    						url: baseLink + res.data.children[i].data.permalink,
+    						url: res.data.children[i].data.url,
     						after: res.data.after
     					})
 
     				}
-    				if (remained > 25) customRequest(catg, remained - 25, res.data.after)
+
+    				if (remained > 25) postsCustomRequest(catg, remained - 25, res.data.after)
+                    //console.log(remained)
 
     				// if (catg.posts) {
     				// 	console.log(catg)
@@ -68,8 +46,52 @@ function customRequest (catg, remained, after) {
 
     				fs.writeFile(path + catg.title + '.html', html, (err, data) => {
     					if(err) console.log(err.message)
+                        //    else console.log(catg.title)
     				})
     			}
     		})
 }
 
+function catgsCustomRequest(remained, after) {
+    request(baseLink + '/subreddits.json?after=' + after, (error, response, body) => {
+
+  if (!error && response.statusCode == 200) {
+    clearFolder(path)
+    let res = JSON.parse(body)
+
+    let k = remained > 25 ? 25: remained
+    //console.log(remained)
+
+    for (let i = 0; i < k; i++) {
+        catgs.push({
+            title: res.data.children[i].data.url.split('/')[2],
+            url: baseLink + res.data.children[i].data.url,
+            posts: [],
+            after: res.data.after
+        })
+    }
+
+    if (remained > 25) catgsCustomRequest(remained - 25, res.data.after)
+
+    if (catgs) {
+        //console.log(catgs)
+        
+        for (let catg of catgs) {
+            postsCustomRequest(catg, postsCount, '')
+        }
+    }
+  } else {
+    console.log("Couldnt' connect\n" + error.message)
+  }
+})
+}
+
+function clearFolder(path) {
+    fs.removeSync(path)
+
+    fs.mkdirs(path, function (err) {
+        if (err) return console.error(err)
+        console.log(path + " created!")
+    })
+    
+}
