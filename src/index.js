b10f4b0785e500b80b90351dadd4ec71541d3c2e
Alexandr Metreniuc <metreniuk@gmail.com>
2016-12-06 17:08:10 +0200

    added posts recursion

--- src/index.js
+++ src/index.js
@@ -12,14 +12,12 @@ request(baseLink + '/subreddits.json', (error, response, body) => {
 
   if (!error && response.statusCode == 200) {
     let res = JSON.parse(body)
-    if (catgsCount > 25) {
-
-    }
     for (let i = 0; i < catgsCount; i++) {
     	catgs.push({
     		title: res.data.children[i].data.title,
     		url: baseLink + res.data.children[i].data.url,
-    		posts: []
+    		posts: [],
+    		after: res.data.after
     	})
     }
 
@@ -27,38 +25,51 @@ request(baseLink + '/subreddits.json', (error, response, body) => {
     	//console.log(catgs)
     	
     	for (let catg of catgs) {
-    		request(catg.url + '.json', (error, response, body) => {
-    			if (!error && response.statusCode == 200) {
-    				let res = JSON.parse(body)
-
-    				for (let i = 0; i < postsCount; i++) {
-    					catg.posts.push({
-    						title: res.data.children[i].data.title,
-    						url: baseLink + res.data.children[i].data.permalink
-    					})
-    				}
-
-    				if (catg.posts) {
-    					console.log(catg)
-    				}
-
-    				let data = ""
-    				let html = `<html><head><title>${catg.title}</title><body><ul>`
-
-    				for (let post of catg.posts) {
-    					data += JSON.stringify(post)
-    					html +=`<li><a href="${post.url}">${post.title}</a></li>`
-    				}
-
-    				fs.writeFile(path + catg.title + '.html', html, (err, data) => {
-    					if(err) console.log(err.message)
-    				})
-    			}
-    		})
+    		customRequest(catg, postsCount, '')
     	}
     }
   } else {
   	console.log("Couldnt' connect\n" + error.message)
   }
 })
+
+function customRequest (catg, remained, after) {
+	console.log(catg.url + '.json?after=' + after)
+	request(catg.url + '.json?after=' + after, (error, response, body) => {
+    			if (!error && response.statusCode == 200) {
+    				let res = JSON.parse(body)
+    				
+    				let k = remained > 25 ? 25: remained
+    				console.log(remained)
+
+    				for (let i = 0; i < k; i++) {
+    					
+    					catg.posts.push({
+    						title: res.data.children[i].data.title,
+    						url: baseLink + res.data.children[i].data.permalink,
+    						after: res.data.after
+    					})
+
+    				}
+    				if (remained > 25) customRequest(catg, remained - 25, res.data.after)
+
+    				// if (catg.posts) {
+    				// 	console.log(catg)
+    				// }
+
+    				let data = ""
+    				let html = `<html><head><title>${catg.title}</title><body><ol>`
+
+    				for (let post of catg.posts) {
+    					data += JSON.stringify(post)
+    					html +=`<li><a href="${post.url}">${post.title}</a></li>`
+    				}
+    				html += `</ol></body></html>`
+
+    				fs.writeFile(path + catg.title + '.html', html, (err, data) => {
+    					if(err) console.log(err.message)
+    				})
+    			}
+    		})
+}
 
