ae0182a391e5ae5b9325b87c0cd6f2b0d62f8cb6
Alexandr Metreniuc <metreniuk@gmail.com>
2016-12-07 13:02:31 +0200

    scaled parser (there is some leaks anyway)

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,9 @@ var baseLink = 'https://www.reddit.com';
 var catgsCount = process.argv[2];
 var postsCount = process.argv[3];
 var catgs = [];
+var catgsCache = [];
 var posts = [];
+var postsCache = [];
 
 clearFolder(path);
 catgsCustomRequest(catgsCount, '');
@@ -23,55 +25,23 @@ function postsCustomRequest(catg, remained, after) {
             //console.log(remained)
 
             for (var i = 0; i < k; i++) {
+                if (i >= res.data.children.length) {
+                    //console.log('There is no such amount of posts in ' + catg.title + ', only ' + res.data.children.length + ' remained')
+                    console.log(catg.title + ': ' + catg.posts.length + ' posts');
+                    writeInCatg(catg);
+                    return;
+                }
                 catg.posts.push({
                     title: res.data.children[i].data.title,
                     url: res.data.children[i].data.url,
                     after: res.data.after
                 });
+                postsCache.push(res.data.children[i].data.title);
             }
 
-            if (remained > 25) postsCustomRequest(catg, remained - 25, res.data.after);
-            //console.log(remained)
-
-            // if (catg.posts) {
-            // 	console.log(catg)
-            // }
-
-            var data = "";
-            var html = '<html><head><title>' + catg.title + '</title><body><ol>';
-
-            var _iteratorNormalCompletion = true;
-            var _didIteratorError = false;
-            var _iteratorError = undefined;
-
-            try {
-                for (var _iterator = catg.posts[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                    var post = _step.value;
-
-                    data += JSON.stringify(post);
-                    html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
-                }
-            } catch (err) {
-                _didIteratorError = true;
-                _iteratorError = err;
-            } finally {
-                try {
-                    if (!_iteratorNormalCompletion && _iterator.return) {
-                        _iterator.return();
-                    }
-                } finally {
-                    if (_didIteratorError) {
-                        throw _iteratorError;
-                    }
-                }
+            if (remained > 25) postsCustomRequest(catg, remained - 25, res.data.after);else {
+                writeInCatg(catg);
             }
-
-            html += '</ol></body></html>';
-
-            fs.writeFile(path + catg.title + '.html', html, function (err, data) {
-                if (err) console.log(err.message);
-                //    else console.log(catg.title)
-            });
         }
     });
 }
@@ -80,13 +50,16 @@ function catgsCustomRequest(remained, after) {
     request(baseLink + '/subreddits.json?after=' + after, function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
-
             var res = JSON.parse(body);
 
             var k = remained > 25 ? 25 : remained;
-            //console.log(remained)
+            //console.log(remained
 
             for (var i = 0; i < k; i++) {
+                if (k > res.data.children.length) {
+                    console.log('There is no such amount of categories');
+                    return;
+                }
                 catgs.push({
                     title: res.data.children[i].data.url.split('/')[2],
                     url: baseLink + res.data.children[i].data.url,
@@ -95,32 +68,33 @@ function catgsCustomRequest(remained, after) {
                 });
             }
 
-            if (remained > 25) catgsCustomRequest(remained - 25, res.data.after);
+            if (remained > 25) catgsCustomRequest(remained - 25, res.data.after);else {
+                console.log('remained ' + remained);
+                if (catgs) {
+                    //console.log(catgs)
 
-            if (catgs) {
-                //console.log(catgs)
+                    var _iteratorNormalCompletion = true;
+                    var _didIteratorError = false;
+                    var _iteratorError = undefined;
 
-                var _iteratorNormalCompletion2 = true;
-                var _didIteratorError2 = false;
-                var _iteratorError2 = undefined;
-
-                try {
-                    for (var _iterator2 = catgs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                        var catg = _step2.value;
-
-                        postsCustomRequest(catg, postsCount, '');
-                    }
-                } catch (err) {
-                    _didIteratorError2 = true;
-                    _iteratorError2 = err;
-                } finally {
                     try {
-                        if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                            _iterator2.return();
+                        for (var _iterator = catgs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                            var catg = _step.value;
+
+                            postsCustomRequest(catg, postsCount, '');
                         }
+                    } catch (err) {
+                        _didIteratorError = true;
+                        _iteratorError = err;
                     } finally {
-                        if (_didIteratorError2) {
-                            throw _iteratorError2;
+                        try {
+                            if (!_iteratorNormalCompletion && _iterator.return) {
+                                _iterator.return();
+                            }
+                        } finally {
+                            if (_didIteratorError) {
+                                throw _iteratorError;
+                            }
                         }
                     }
                 }
@@ -132,10 +106,75 @@ function catgsCustomRequest(remained, after) {
 }
 
 function clearFolder(path) {
-    fs.removeSync(path);
+    fs.emptyDir(path, function (err) {
+        if (err) console.log(err.message);
+        console.log(path + ' cleared');
+    });
+}
 
-    fs.mkdirs(path, function (err) {
-        if (err) return console.error(err);
-        console.log(path + " created!");
+function checkCacheTitle(title, a) {
+    var k = false;
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+        for (var _iterator2 = a[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var el = _step2.value;
+
+            if (title == el.title) k = true;
+        }
+    } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                _iterator2.return();
+            }
+        } finally {
+            if (_didIteratorError2) {
+                throw _iteratorError2;
+            }
+        }
+    }
+
+    return k;
+}
+
+function writeInCatg(catg) {
+    var data = "";
+    var html = '<html><head><title>' + catg.title + '</title><body><ol>';
+
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+        for (var _iterator3 = catg.posts[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+            var post = _step3.value;
+
+            data += JSON.stringify(post);
+            html += '<li><a href="' + post.url + '">' + post.title + '</a></li>';
+        }
+    } catch (err) {
+        _didIteratorError3 = true;
+        _iteratorError3 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion3 && _iterator3.return) {
+                _iterator3.return();
+            }
+        } finally {
+            if (_didIteratorError3) {
+                throw _iteratorError3;
+            }
+        }
+    }
+
+    html += '</ol></body></html>';
+
+    fs.writeFile(path + catg.title + '.html', html, function (err, data) {
+        if (err) console.log(err.message);else console.log('wrote in ' + catg.title, postsCache.length);
     });
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ let baseLink = 'https://www.reddit.com';
 let catgsCount = process.argv[2]
 let postsCount = process.argv[3]
 let catgs = []
+let catgsCache = []
 let posts = []
+let postsCache = []
 
 clearFolder(path)
 catgsCustomRequest(catgsCount, '')
@@ -20,35 +22,29 @@ function postsCustomRequest (catg, remained, after) {
     				let k = remained > 25 ? 25: remained
     				//console.log(remained)
 
-    				for (let i = 0; i < k; i++) {
-    					catg.posts.push({
-    						title: res.data.children[i].data.title,
-    						url: res.data.children[i].data.url,
-    						after: res.data.after
-    					})
-
-    				}
+                    for (let i = 0; i < k; i++) {
+                        if (i >= res.data.children.length) {
+                            //console.log('There is no such amount of posts in ' + catg.title + ', only ' + res.data.children.length + ' remained')
+                            console.log(catg.title + ': ' + catg.posts.length + ' posts')
+                            writeInCatg(catg)
+                            return
+                        }
+                        catg.posts.push({
+                            title: res.data.children[i].data.title,
+                            url: res.data.children[i].data.url,
+                            after: res.data.after
+                        })
+                        postsCache.push(res.data.children[i].data.title)
+                        
+                    }
+    				
 
     				if (remained > 25) postsCustomRequest(catg, remained - 25, res.data.after)
-                    //console.log(remained)
+                        else {
+                            writeInCatg(catg)
+                        }
 
-    				// if (catg.posts) {
-    				// 	console.log(catg)
-    				// }
-
-    				let data = ""
-    				let html = `<html><head><title>${catg.title}</title><body><ol>`
-
-    				for (let post of catg.posts) {
-    					data += JSON.stringify(post)
-    					html +=`<li><a href="${post.url}">${post.title}</a></li>`
-    				}
-    				html += `</ol></body></html>`
-
-    				fs.writeFile(path + catg.title + '.html', html, (err, data) => {
-    					if(err) console.log(err.message)
-                        //    else console.log(catg.title)
-    				})
+    				
     			}
     		})
 }
@@ -57,13 +53,16 @@ function catgsCustomRequest(remained, after) {
     request(baseLink + '/subreddits.json?after=' + after, (error, response, body) => {
 
   if (!error && response.statusCode == 200) {
-
     let res = JSON.parse(body)
 
     let k = remained > 25 ? 25: remained
-    //console.log(remained)
+    //console.log(remained
 
     for (let i = 0; i < k; i++) {
+        if (k > res.data.children.length) {
+            console.log('There is no such amount of categories')
+            return
+        }
         catgs.push({
             title: res.data.children[i].data.url.split('/')[2],
             url: baseLink + res.data.children[i].data.url,
@@ -73,14 +72,18 @@ function catgsCustomRequest(remained, after) {
     }
 
     if (remained > 25) catgsCustomRequest(remained - 25, res.data.after)
-
-    if (catgs) {
-        //console.log(catgs)
-        
-        for (let catg of catgs) {
-            postsCustomRequest(catg, postsCount, '')
+        else {
+            console.log('remained ' + remained)
+            if (catgs) {
+                //console.log(catgs)
+                
+                for (let catg of catgs) {
+                    postsCustomRequest(catg, postsCount, '')
+                }
+            }
         }
-    }
+
+    
   } else {
     console.log("Couldnt' connect\n" + error.message)
   }
@@ -88,11 +91,34 @@ function catgsCustomRequest(remained, after) {
 }
 
 function clearFolder(path) {
-    fs.removeSync(path)
-
-    fs.mkdirs(path, function (err) {
-        if (err) return console.error(err)
-        console.log(path + " created!")
+    fs.emptyDir(path, (err) => {
+        if (err) console.log(err.message)
+        console.log(path + ' cleared')
     })
     
+}
+
+function checkCacheTitle(title, a) {
+    let k = false
+    for (let el of a) {
+        if (title == el.title)
+            k = true
+    }
+    return k
+}
+
+function writeInCatg(catg) {
+    let data = ""
+    let html = `<html><head><title>${catg.title}</title><body><ol>`
+
+    for (let post of catg.posts) {
+        data += JSON.stringify(post)
+        html +=`<li><a href="${post.url}">${post.title}</a></li>`
+    }
+    html += `</ol></body></html>`
+
+    fs.writeFile(path + catg.title + '.html', html, (err, data) => {
+        if(err) console.log(err.message)
+            else console.log('wrote in ' + catg.title, postsCache.length)
+    })
 }
